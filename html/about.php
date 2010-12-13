d4f9613a4e52cc8847e04240f8294745946181ed
Kfir Dolev <bazilikan@gmail.com>
2010-12-12 18:22:36 -0800

    added navbar and projects, members, and about pages

---
+++ html/about.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+	<head>
+		<title>Project Club | About</title>
+		<link rel="stylesheet" type="text/css" href="../css/base.css" MEDIA"screen" />
+	</head>
+	<body>
+		<h1>About</h1>
+			<ul id="navbar">
+				<li class="navbar"><a href="../index.php">Home</a></li>
+				<li class="navbar"><a href="projects.php">Projects</a></li>
+				<li class="navbar"><a href="members.php">Members</a></li>
+				<li class="navbar"><a href="about.php">About</a></li>
+			</ul>
+		<p>This is for Practicing Purposes</p>
+	</body>
+</html>
---
+++ html/members.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+	<head>
+		<title>Project Club | Members</title>
+		<link rel="stylesheet" type="text/css" href="../css/base.css" />
+	</head>
+	<body>
+		<h1>Members</h1>
+			<ul id="navbar">
+				<li class="navbar"><a href="../index.php">Home</a></li>
+				<li class="navbar"><a href="projects.php">Projects</a></li>
+				<li class="navbar"><a href="members.php">Members</a></li>
+				<li class="navbar"><a href="about.php">About</a></li>
+			</ul>
+		<p>This is for Practicing Purposes</p>
+	</body>
+</html>
