6bcc202066c647fc029085d37bca8eb59d1c4c20
Kfir Dolev <bazilikan@gmail.com>
2011-01-22 21:12:05 -0800

    redid everything

--- index.php
+++ index.php
@@ -1,20 +1,34 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-	<head>
-		<title>Project Club | Welcome</title>
-		<link rel="stylesheet" type="text/css" href="css/base.css" />
-	</head>
-	<body>
-		<h1>Projects Club</h1>
-			<ul id="navbar">
-					<?php
-					include("navbar.php");
-					?>
-			</ul>
-		<div id="header1">
-			<img src="pictures/projectsclub1.jpg" width=300px/>
-			<img src="pictures/projectsclub2.jpg" width=300px/>
-		</div>
-	</body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	
+	<link rel="stylesheet" type="text/css" href="css/base.css" />
+
+	<title>Home</title>
+	
+</head>
+
+<body>
+
+<div class="banner">
+	<h1 class="banner">Projects Club</h1>
+</div>
+
+<div id="navbar">
+	<ul>
+		<li class="navbar"><a class="navbar" href="">About</a></li>
+		<li class="navbar"><a class="navbar" href="">Projects</a></li>
+		<li class="navbar"><a class="navbar" href="">Members</a></li>
+		<li class="navbar"><a class="navbar" href="">Program-a-thon</a></li>
+		<li class="navbar"><a class="navbar" href="">Contact Us</a></li>
+	<ul>
+</div>
+		
+
+</body>
 </html>
