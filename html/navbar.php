8e28cac32f34aaa4960b20a196335b4d921465b2
Kfir Dolev <bazilikan@gmail.com>
2011-01-23 18:20:25 -0800

    changed the whole thing again, this time harvard style

--- html/navbar.php
+++
@@ -1,4 +0,0 @@
-<li class="navbar"><a href="../index.php">Home</a></li>
-<li class="navbar"><a href="projects.php">Projects</a></li>
-<li class="navbar"><a href="members.php">Members</a></li>
-<li class="navbar"><a href="about.php">About</a></li>
--- index.php
+++ index.php
@@ -2,33 +2,37 @@
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
-
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	
 	<link rel="stylesheet" type="text/css" href="css/base.css" />
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
 	<title>Home</title>
 	
 </head>
 
 <body>
-
-<div class="banner">
-	<h1 class="banner">Projects Club</h1>
-</div>
-
-<div id="navbar">
-	<ul>
-		<li class="navbar"><a class="navbar" href="">About</a></li>
-		<li class="navbar"><a class="navbar" href="">Projects</a></li>
-		<li class="navbar"><a class="navbar" href="">Members</a></li>
-		<li class="navbar"><a class="navbar" href="">Program-a-thon</a></li>
-		<li class="navbar"><a class="navbar" href="">Contact Us</a></li>
-	<ul>
-</div>
+	<div id="header">
 		
+		<div id="logo">
+			<img class="logo" src="pictures/logofirst.gif" />
+			<img class="logo" src="pictures/logoend.gif" />
+		</div>
+		
+		<div id="navbar">
+			<ul>
+				<li class="navbar"><a class="navbar" href="">About</a></li>
+				<li class="navbar"><a class="navbar" href="">Projects</a></li>
+				<li class="navbar"><a class="navbar" href="">Members</a></li>
+				<li class="navbar"><a class="navbar" href="">Program-a-thons</a></li>
+				<li class="navbar"><a class="navbar" href="">Contact Us</a></li>
+			</ul>
+		</div>
+		<div class="clear"></div>		
+		<div id="line"></div>
+		
+	</div>
 
 </body>
 </html>
