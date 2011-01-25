bb479a5755d15b6708ab2d4550e2466cbc8ca31c
Kfir Dolev <bazilikan@gmail.com>
2011-01-24 18:21:16 -0800

    added some content

---
+++ html/about.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	
+	<link rel="stylesheet" type="text/css" href="../css/base.css" />
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+	<title>Home</title>
+	
+</head>
+
+<body>
+	<div id="header">
+		
+		<div id="logo">
+			<a href="../index.php">
+				<img class="logo" src="../pictures/logofirst.gif" />
+				<img class="logo" src="../pictures/logoend.gif" />
+			</a>
+		</div>
+		
+		<div id="navbar">
+			<ul>
+				<li class="navbar"><a class="navbar" href="about.php">About</a></li>
+				<li class="navbar"><a class="navbar" href="projects.php">Projects</a></li>
+				<li class="navbar"><a class="navbar" href="members.php">Members</a></li>
+				<li class="navbar"><a class="navbar" href="program_a_thons.php">Program-a-thons</a></li>
+				<li class="navbar"><a class="navbar" href="contact_us.php">Contact Us</a></li>
+			</ul>
+		</div>
+		<div class="clear"></div>		
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -16,21 +16,53 @@
 	<div id="header">
 		
 		<div id="logo">
-			<img class="logo" src="pictures/logofirst.gif" />
-			<img class="logo" src="pictures/logoend.gif" />
+			<a href="index.php">
+				<img class="logo" src="pictures/logofirst.gif" />
+				<img class="logo" src="pictures/logoend.gif" />
+			</a>
 		</div>
 		
 		<div id="navbar">
 			<ul>
-				<li class="navbar"><a class="navbar" href="">About</a></li>
-				<li class="navbar"><a class="navbar" href="">Projects</a></li>
-				<li class="navbar"><a class="navbar" href="">Members</a></li>
-				<li class="navbar"><a class="navbar" href="">Program-a-thons</a></li>
-				<li class="navbar"><a class="navbar" href="">Contact Us</a></li>
+				<li class="navbar"><a class="navbar" href="html/about.php">About</a></li>
+				<li class="navbar"><a class="navbar" href="html/projects.php">Projects</a></li>
+				<li class="navbar"><a class="navbar" href="html/members.php">Members</a></li>
+				<li class="navbar"><a class="navbar" href="html/program_a_thons.php">Program-a-thons</a></li>
+				<li class="navbar"><a class="navbar" href="html/contact_us.php">Contact Us</a></li>
 			</ul>
 		</div>
 		<div class="clear"></div>		
 		<div id="line"></div>
+		
+	</div>
+	
+	<div id="home_feature">
+		<img class="home_feature" src="pictures/paly.jpg" />
+		<div id="home_feature_description_outer">
+			<div id="home_feature_description_inner">
+				<h1 class="home_feature">Paly</h1>
+				<p class="home_feature"> The Paly Projects Club is organized to help young students at Palo Alto High School to accomplish and start their ideas. At the Projects Club we provide the necessary tools and resources, to the students, which will help them with their idea.  </p>
+			</div>
+		</div>
+	</div>
+	
+	<div class="clear"></div>
+	
+	<div id="other_stuff">
+		
+		<div id="home_news">
+			<h2 id="news_header">News</h2>
+				<div id="news">
+					<img id="news_image" src="pictures/programathon1.jpg" />
+					<div id="news_text">
+						<p class="standard_text">Saturday, January 23 we had our first ever program-a-thon at the paly robotics room. After programming from 1pm to 4:30pm, Team 2 (Daniel Sooner, Akash Gupta, Peter Kotik, and Sergey Smirnov) won with their math guessing game. <a class="read_more" href="">Read More...</a></p>
+					</div>
+				</div>
+		</div>
+		
+		<div id="current_projects">
+			<h2 id="current_projects_header">Current Projects</h2>
+		</div>	
 		
 	</div>
 
